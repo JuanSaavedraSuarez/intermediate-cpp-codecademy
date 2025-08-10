a5cdf95d8ef70546c42ba1195ce836f4d84080c3
j s <jlssz@proton.me>
2025-08-09 21:58:24 -0700

    Added testing in main

--- departmental_dashboard/Sales.hpp
+++ departmental_dashboard/Sales.hpp
@@ -1,3 +1,51 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+using std::string;
+using std::cout;
+
+namespace Sales {
+  class Team {
+  private:
+    string teamName;
+    std::vector<string> teamMembers;
+    string teamLocation;
+  public:
+    Team(string name, std::vector<string> members, string location) 
+      : teamName { name }
+      , teamMembers { members }
+      , teamLocation { location }
+      {}
+
+    inline string getTeamName() { return teamName ; }
+
+    inline std::vector<string> getTeamMembers() { return teamMembers; }
+
+    inline void addMember(string member) {
+      teamMembers.push_back(member);
+      return;
+    }
+
+    inline string getLocation() { return teamLocation; }
+
+    inline void setLocation(string& location) {
+      teamLocation = location;
+      return;
+    }
+
+    inline void printTeamDetails() {
+      cout << "Sales Team Name: " << getTeamName()
+        << "Location: " << getLocation() 
+        << "Members: ";
+      
+      for (string member : teamMembers) {
+        cout << member;
+        if (member != teamMembers.back())
+          cout << ", ";
+        else
+          cout << std::endl;
+      }
+    }
+  };
+}
--- departmental_dashboard/main.cpp
+++ departmental_dashboard/main.cpp
@@ -2,8 +2,32 @@
 #include "Marketing.hpp"
 #include "Sales.hpp"
 
+namespace audit = Marketing::Support;
 
 int main() {
+  std::vector<string> marketingMembers, salesMembers;
+  
+  Marketing::Team* marketingTeam 
+    = new Marketing::Team("Stellar Corp", marketingMembers, 500000);
+  marketingTeam->addMember("Scott");
+  marketingTeam->addMember("Nicole");
+  marketingTeam->addMember("Artem");
 
- return 0;
+  Sales::Team* salesTeam 
+    = new Sales::Team("Stellar Corp", salesMembers, "London");
+  salesTeam->addMember("Patrik");
+  salesTeam->addMember("Emily");
+  salesTeam->addMember("Clive");
+
+  string newLocation = "New York";
+  salesTeam->printTeamDetails();
+  salesTeam->setLocation(newLocation);
+  salesTeam->printTeamDetails();
+  
+  audit::auditTeam(*marketingTeam);
+
+  delete marketingTeam;
+  delete salesTeam;
+
+  return 0;
 }
